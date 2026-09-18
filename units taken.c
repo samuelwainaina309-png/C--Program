0ec3bd122cfff6103e49e48ca41b77821eca992e
samuelwainaina309-png <samuelwainaina309@gmail.com>
2026-09-18 00:02:28 -0700

    Add files via upload

---
+++ units taken.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    printf("Analogue electronics.\n");
+    printf("Essentials of Accounting.\n");
+    printf("Mathematics for Science.\n");
+    printf("Fundamentals of Digital Technology and Artificial.\n");
+    printf("Introduction to computer Applications.\n");
+    printf("Computer Systems and Organization.\n");
+    printf("Introduction to Programming and Algorithm.\n");
+    printf("Communication and Information Literacy Skills.\n");
+    return 0;
+}
